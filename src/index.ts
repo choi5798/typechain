6b908c832ba31c75a0c84cb72ece9bed86bbc413
Jerry <sw.choi@khu.ac.kr>
2022-02-23 21:08:44 +0900

    Interfaces on Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,20 @@
-const sayHi = (name:string, age:number, gender:string):string => {
-    return (`Hello ${name}, you are ${age}, you are a ${gender}`)
+interface Human{ // interface는 ts 에서만 동작함, js에서는 동작 X
+    // 예) 블록체인에서 한 블록을 interface로 정의함
+    name:string;
+    age:number;
+    gender:string;
 }
 
-console.log(sayHi("Jerry", 444, "male"))
+const person = {
+    name: "Jerry",
+    age: 24,
+    gender: "male"
+}
+
+const sayHi = (person:Human):string => {
+    return (`Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`)
+}
+
+console.log(sayHi(person))
 
 export {};
